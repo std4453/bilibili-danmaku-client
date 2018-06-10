728ad2241353a99e3c390871d9f63132ef1443d5
Harry Zhang <std4453@foxmail.com>
2018-06-10 22:54:13 +0800

    Add simple test index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 require('dotenv').config();
 
-const { on, keepAlive } = require('./live');
+const DanmakuClient = require('./DanmakuClient');
+
+const run = () => new Promise((resolve) => {
+    const client = new DanmakuClient({
+        room: 1,
+    });
+    client.start();
+    client.on('stateChange', (newState) => {
+        if (newState === 'terminated') resolve();
+    });
+});
+
+(async () => {
+    await run();
+})();
