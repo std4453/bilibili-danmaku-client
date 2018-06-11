848adecfbade982a4c90196d199597f9a8c87b5a
Harry Zhang <std4453@foxmail.com>
2018-06-11 13:42:15 +0800

    Use wallaby.js

---
+++ wallaby.js
@@ -0,0 +1,12 @@
+module.exports = () => ({
+    files: [
+        'src/**/*.js',
+    ],
+    tests: [
+        'test/**/*.js',
+    ],
+    env: {
+        type: 'node',
+    },
+    debug: true,
+});
