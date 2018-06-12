a6ea43eb25710f2d11aa482b93f2efdf5d013202
Harry Zhang <std4453@foxmail.com>
2018-06-12 19:49:57 +0800

    Add test index.js It will be unable to build by now.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import debug from 'debug';
+import DanmakuClient from '@std4453/bilibili-danmaku-client';
 
 localStorage.debug = '*';
 const log = debug('bilibili-danmaku-client-site');
-log('Hello, webpack!');
+
+const client = new DanmakuClient();
+client.start();
+
+client.on('danmaku', event => log(`${event.sender.name}: ${event.content}`));
+client.on('sysMsg', event => log(event.msgText));
+client.on('gift', event => log(`${event.sender.name} => ${event.giftName} * ${event.num}`));
+client.on('vipEnter', event => log(`welcome vip ${event.name}`));
+client.on('guardEnter', event => log(`welcome guard ${event.name}`));
+client.on('comboEnd', event => log(`[all] ${event.name} => ${event.giftName} * ${event.comboNum}`));
