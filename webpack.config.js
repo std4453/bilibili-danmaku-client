8ea62f1d8c09f22f07636711f0d186e7c1964754
Harry Zhang <std4453@foxmail.com>
2018-06-15 09:19:31 +0800

    Install vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const dev = !!process.env.WEBPACK_SERVE;
 module.exports = {
@@ -12,12 +13,33 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                loader: ['babel-loader', 'eslint-loader'],
+                use: ['babel-loader', 'eslint-loader'],
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 },
+                    },
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader'],
             },
         ],
     },
-    plugins: dev ? [] : [
-        new CleanWebpackPlugin(['dist']),
+    plugins: dev ? [
+        new VueLoaderPlugin(),
+    ] : [
+        new VueLoaderPlugin(), new CleanWebpackPlugin(['dist']),
     ],
     devtool: 'source-map',
     target: 'web',
