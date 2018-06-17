db3429a88f2328b330af3fe8aec309879d0f7e90
Harry Zhang <std4453@foxmail.com>
2018-06-17 21:31:58 +0800

    Add documentation for WebSocketConnection.js

--- src/transport/WebSocketConnection.js
+++ src/transport/WebSocketConnection.js
@@ -2,7 +2,17 @@ const WebSocket = require('x-platform-ws');
 
 const { CascadeConnection } = require('../util/connection');
 
+/**
+ * BaseConnection implementation wrapping a WebSocket from x-platform-ws.
+ * Since BaseConnection imitates the API of ws (which x-platform-ws internally
+ * uses), the wrapping is as simple as putting an elephant into a refridgerator.
+ */
 class WebSocketConnection extends CascadeConnection {
+    /**
+     * Construct a new WebSocketConnection.
+     * All arguments are passed onto WebSocket for initialization.
+     * @param {any[]} args Arguments to pass to new WebSocket().
+     */
     constructor(...args) {
         super(new WebSocket(...args));
     }
